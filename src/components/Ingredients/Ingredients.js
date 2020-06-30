9ceeb0f2d9e73ba013e0689430c6095cf5b251de
lParanhos <leandro.paranhos@dextra-sw.com>
2020-06-30 10:05:34 -0300

    using custom hooks

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,9 +1,9 @@
 import React, { useReducer, useEffect, useCallback, useMemo } from "react";
 
 import IngredientForm from "./IngredientForm";
-import Search from "./Search";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
+import Search from "./Search";
 import useHttp from "../../hooks/http";
 
 const ingredientReducer = (currentIngredients, action) => {
@@ -13,51 +13,64 @@ const ingredientReducer = (currentIngredients, action) => {
     case "ADD":
       return [...currentIngredients, action.ingredient];
     case "DELETE":
-      return currentIngredients.filter(
-        (ingredient) => ingredient.id !== action.id
-      );
+      return currentIngredients.filter((ing) => ing.id !== action.id);
     default:
-      throw new Error("Shold not get there");
+      throw new Error("Should not get there!");
   }
 };
 
-function Ingredients() {
+const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  const { isLoading, sendRequest, error, data } = useHttp();
+  const {
+    isLoading,
+    error,
+    data,
+    sendRequest,
+    reqExtra,
+    reqIdentifer,
+    clear,
+  } = useHttp();
 
   useEffect(() => {
-    console.log("RENDERING INGREDIENTS", userIngredients);
-  }, [userIngredients]);
+    if (!isLoading && !error && reqIdentifer === "REMOVE_INGREDIENT") {
+      dispatch({ type: "DELETE", id: reqExtra });
+    } else if (!isLoading && !error && reqIdentifer === "ADD_INGREDIENT") {
+      dispatch({
+        type: "ADD",
+        ingredient: { id: data.name, ...reqExtra },
+      });
+    }
+  }, [data, reqExtra, reqIdentifer, isLoading, error]);
 
-  const addIngredientHandler = useCallback((ingredient) => {
-    /* setIsLoading(true); */
-    /*  setUserIngredients(prevIngredients => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient}
-      ]); */
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    dispatch({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
-  const removeIngredientHandler = useCallback(
-    (ingredientId) => {
-      /* setIsLoading(true); */
+  const addIngredientHandler = useCallback(
+    (ingredient) => {
       sendRequest(
-        `${process.env.REACT_APP_FIREBASE_URL}/ingredients/${ingredientId}.json`,
-        "DELETE"
+        `${process.env.REACT_APP_FIREBASE_URL}/ingredients.json`,
+        "POST",
+        JSON.stringify(ingredient),
+        ingredient,
+        "ADD_INGREDIENT"
       );
     },
     [sendRequest]
   );
 
-  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    //setUserIngredients(filteredIngredients)
-    dispatch({ type: "SET", ingredients: filteredIngredients });
-  }, []);
-
-  const clearError = useCallback(() => {
-    /* setError(null);
-    setIsLoading(false); */
-    // dispatchHttp({ type: "CLEAR" });
-  }, []);
+  const removeIngredientHandler = useCallback(
+    (ingredientId) => {
+      sendRequest(
+        `${process.env.REACT_APP_FIREBASE_URL}/ingredients/${ingredientId}.json`,
+        "DELETE",
+        null,
+        ingredientId,
+        "REMOVE_INGREDIENT"
+      );
+    },
+    [sendRequest]
+  );
 
   const ingredientList = useMemo(() => {
     return (
@@ -70,17 +83,19 @@ function Ingredients() {
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={isLoading}
       />
+
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
         {ingredientList}
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
--- src/hooks/http.js
+++ src/hooks/http.js
@@ -1,62 +1,82 @@
 import { useReducer, useCallback } from "react";
 
+const initialState = {
+  loading: false,
+  error: null,
+  data: null,
+  extra: null,
+  identifier: null,
+};
+
 const httpReducer = (curHttpState, action) => {
   switch (action.type) {
     case "SEND":
-      return { loading: true, error: false };
+      return {
+        loading: true,
+        error: null,
+        data: null,
+        extra: null,
+        identifier: action.identifier,
+      };
     case "RESPONSE":
       return {
         ...curHttpState,
         loading: false,
-        responseData: action.responseData,
+        data: action.responseData,
+        extra: action.extra,
       };
     case "ERROR":
       return { loading: false, error: action.errorMessage };
     case "CLEAR":
-      return { ...curHttpState, error: null };
+      return initialState;
     default:
-      throw new Error("Shold not be reached");
+      throw new Error("Should not be reached!");
   }
 };
 
 const useHttp = () => {
-  const [httpState, dispatchHttp] = useReducer(httpReducer, {
-    loading: false,
-    error: null,
-    responseData: null,
-  });
+  const [httpState, dispatchHttp] = useReducer(httpReducer, initialState);
 
-  const sendRequest = useCallback((url, method, body) => {
-    dispatchHttp({ type: "SEND" });
-    fetch(url, {
-      method: method,
-      body: body,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        return response.json();
-        /* setIsLoading(false); */
-        /*  setUserIngredients(prevIngredients => 
-            prevIngredients.filter(ing => ing.id !== ingredientId)
-          ) */
-        // dispatch({ type: "DELETE", id: ingredientId });
+  const clear = useCallback(() => dispatchHttp({ type: "CLEAR" }), []);
+
+  const sendRequest = useCallback(
+    (url, method, body, reqExtra, reqIdentifer) => {
+      dispatchHttp({ type: "SEND", identifier: reqIdentifer });
+      fetch(url, {
+        method: method,
+        body: body,
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .then((responseData) => {
-        dispatchHttp({ type: "RESPONSE", responseData });
-      })
-      .catch((err) => {
-        /* setError("Something went wrong"); */
-        dispatchHttp({ type: "ERROR", errorMessage: "Something went wrong" });
-      });
-  }, []);
+        .then((response) => {
+          return response.json();
+        })
+        .then((responseData) => {
+          dispatchHttp({
+            type: "RESPONSE",
+            responseData: responseData,
+            extra: reqExtra,
+          });
+        })
+        .catch((error) => {
+          dispatchHttp({
+            type: "ERROR",
+            errorMessage: "Something went wrong!",
+          });
+        });
+    },
+    []
+  );
 
   return {
-    isLoading: httpState.isLoading,
+    isLoading: httpState.loading,
+    data: httpState.data,
     error: httpState.error,
-    data: httpState.responseData,
     sendRequest: sendRequest,
+    reqExtra: httpState.extra,
+    reqIdentifer: httpState.identifier,
+    clear: clear,
   };
 };
 
