726144814ad6c3efd94a75d30eaf58f035970db3
lParanhos <leandro.paranhos@dextra-sw.com>
2020-06-30 10:20:49 -0300

    using custom hook

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,51 +1,61 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
+import "./Search.css";
+import useHttp from "../../hooks/http";
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
-  const [enteredFilter, setEnteredFilter] = useState('');
+  const [enteredFilter, setEnteredFilter] = useState("");
   const inputRef = useRef();
+  const { data, isLoading, error, sendRequest, clear } = useHttp();
 
   useEffect(() => {
-   const timer =  setTimeout(() => {
-      if(enteredFilter === inputRef.current.value) {
-        const query = enteredFilter.length === 0 ? '' :
-        `?orderBy="title"&equalTo="${enteredFilter}"`;
-        
-      fetch(`${process.env.REACT_APP_FIREBASE_URL}/ingredients.json` + query)
-        .then(response => response.json())
-        .then(responseData => {
-          const loadedIngredients = [];
-          for(const key in responseData){
-            loadedIngredients.push({
-              id: key,
-              title: responseData[key].title,
-              amount: responseData[key].amount
-            })
-          }
-         onLoadIngredients(loadedIngredients);
-        });
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        sendRequest(
+          `${process.env.REACT_APP_FIREBASE_URL}/ingredients.json` + query,
+          "GET"
+        );
       }
-    }, 500)
+    }, 500);
 
     return () => {
       clearTimeout(timer);
-    }
-  }, [enteredFilter, onLoadIngredients, inputRef])
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef, sendRequest, data]);
 
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredients = [];
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount,
+        });
+      }
+      onLoadIngredients(loadedIngredients);
+    }
+  }, [data, isLoading, error, onLoadIngredients]);
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input 
+          {isLoading && <span>Loading...</span>}
+          <input
             type="text"
             ref={inputRef}
-            value={enteredFilter} 
-            onChange={event => setEnteredFilter(event.target.value)} 
-            />
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
